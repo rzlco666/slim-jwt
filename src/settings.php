0dfe465b9c39f0fe2983da67500d6d1dcb1a9b96
Rizal <syahrizalhanif@gmail.com>
2022-11-22 21:21:53 +0700

    Db setting

--- src/settings.php
+++ src/settings.php
@@ -15,5 +15,14 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+
+        // Database settings
+        'db' => [
+            'host' => 'localhost',
+            'user' => 'root',
+            'pass' => '',
+            'dbname' => 'test_jwt',
+            'driver' => 'mysql'
+        ],
     ],
 ];
